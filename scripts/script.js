125e91dad964c22dd86fc727c8b31f74623e1f42
Marcos Rafael Pereira <rafael_dabarto@hotmail.com>
2021-02-12 21:04:33 -0300

    .

---
+++ scripts/script.js
@@ -0,0 +1,105 @@
+// variaveis globais
+const DATABASE ='Lista'
+const dadoLocal = localStorage.getItem(DATABASE);
+
+// ternario - se dadoLocal tiver conteudo, vai retornar o parse do conteudo. Se não, retorna o objeto vazio.
+var data = dadoLocal ?
+  JSON.parse(dadoLocal):
+  {
+    id:0,
+    list:[]
+  }
+
+var localAnime = document.getElementById("anime");
+var localEpisodio = document.getElementById("episodio");
+var localStatus = document.getElementById("status");
+
+// assim que termina de carregar a página, chama a função render.
+window.addEventListener('load',render())
+
+// Renderiza a lista de registro
+function render(){
+  //pegando o local onde ficará a lista
+  const tbody = document.getElementById('tbody');
+  // limpa o tbody
+  tbody.innerHTML = '';
+  //pecorre os elemento em data e cria uma nova lista
+  data.list.map(function(item){
+    const tr = document.createElement('tr');
+    tr.classList.add("linha")
+    tr.innerHTML = `
+      <td class="hiden">${item.id}</td>
+      <td class="color-anime ">${item.anime}</td>
+      <td class="color-episodio ">${item.episodio}</td>
+      <td class="color-status ">${item.status}</td>
+      <td>
+        <button class="edit " onclick= 'edit(${item.id},"${item.anime}")'>Edit</button>
+        <button class="del " onclick="deleteReg(${item.id})">Delete</button>
+      </td>
+    `
+    // insere a nova tr e seus elemento no tbody
+    tbody.appendChild(tr);
+  })
+}
+
+// adiciona o registro
+function add(){
+  let botao = document.querySelector(".button")
+  
+  data.id++; // incrementa o número do id
+  console.log(data.id)
+
+ //adiciona os elementos do input no array.
+  data.list.push({
+    id:data.id,
+    anime:localAnime.value,
+    episodio:localEpisodio.value,
+    status:localStatus.value,
+   });
+  
+  // limpa os valores do input e da foco no input anime.
+  localAnime.value = "";
+  localEpisodio.value = "";
+  localStatus.value = "";
+  localAnime.focus();
+
+
+  render();
+  save();
+  
+}
+
+// edita o registro
+function edit(id, anime){
+  data.list = data.list.filter(function(item){
+    if(item.id === id){
+      item.anime = anime;
+    }
+    return item
+  })
+    
+  render();
+  save();
+  
+}
+// deleta o registro
+
+function deleteReg(id){
+  if(!confirm('Deseja deletar o anime? ')) return;
+
+  //se o registro for diferente do id do item selecionado, ele se mantem na lista.
+   data.list=data.list.filter(function(item){
+   
+    return item.id !== id
+
+  })
+  
+  render();
+  save();
+}
+
+//salva os dados no localStorage
+function save(){
+  localStorage.setItem(DATABASE, JSON.stringify (data))
+
+}
